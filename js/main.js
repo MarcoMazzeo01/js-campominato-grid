4d34df490710d086d83187313a93879ce2cf9ba7
plastikSun <marcom2k01@gmail.com>
2023-08-21 17:42:41 +0200

    Revert "add: difficulty selector"
    This reverts commit 24d0f7c2bd0ad4cd27197f502c938cf0107da84a.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const startButton = document.getElementById("start_game")
 let selector = document.getElementById("difficulty-selector")
 
 let grid = document.getElementById("grid")
+<<<<<<< HEAD
 let rootRules = document.querySelector(":root")
 
 startButton.addEventListener("click",function() {
@@ -9,11 +10,17 @@ startButton.addEventListener("click",function() {
     let difficulty = selector.value
     rootRules.style.setProperty("--grid-divisor",selector.value)
     generateGrid(difficulty)
+=======
+
+startButton.addEventListener("click",function() {
+    const difficulty = selector.value
+    generateGrid()
+>>>>>>> parent of 24d0f7c (add: difficulty selector)
 })
 
-function generateGrid(difficulty) {
+function generateGrid() {
 
-    for (i = 1; i <= difficulty; i++) {
+    for (i = 1; i <= 100; i++) {
         let numLi = document.createElement("li")
         numLi.innerHTML = i
         numLi.setAttribute("number",i)
