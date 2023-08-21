7cde76c2ab871f28ad3274ab670ecc0d70a8f198
plastikSun <marcom2k01@gmail.com>
2023-08-21 16:59:30 +0200

    add: grid generation and attributes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 const startButton = document.getElementById("start_game")
 let selector = document.getElementById("difficulty-selector")
 
+let grid = document.getElementById("grid")
+
 startButton.addEventListener("click",function() {
     const difficulty = selector.value
-    console.log(difficulty)
+    generateGrid()
 })
+
+function generateGrid() {
+
+    for (i = 1; i <= 100; i++) {
+        let numLi = document.createElement("li")
+        numLi.innerHTML = i
+        numLi.setAttribute("number",i)
+        grid.append(numLi)
+
+        numLi.addEventListener("click",function(){
+            console.log(this.getAttribute("number"))
+        })
+    }
+    
+}
