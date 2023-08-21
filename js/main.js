9b9c007199547e5e0b5fd87ac978489146eda02c
plastikSun <marcom2k01@gmail.com>
2023-08-21 17:45:47 +0200

    add: grid ratio editing based on number of elements in grid

--- js/main.js
+++ js/main.js
@@ -2,25 +2,18 @@ const startButton = document.getElementById("start_game")
 let selector = document.getElementById("difficulty-selector")
 
 let grid = document.getElementById("grid")
-<<<<<<< HEAD
 let rootRules = document.querySelector(":root")
 
 startButton.addEventListener("click",function() {
     grid.innerHTML = ""
     let difficulty = selector.value
     rootRules.style.setProperty("--grid-divisor",selector.value)
-    generateGrid(difficulty)
-=======
-
-startButton.addEventListener("click",function() {
-    const difficulty = selector.value
-    generateGrid()
->>>>>>> parent of 24d0f7c (add: difficulty selector)
+    generateGrid(Math.pow(difficulty,2))
 })
 
-function generateGrid() {
+function generateGrid(difficulty) {
 
-    for (i = 1; i <= 100; i++) {
+    for (i = 1; i <= difficulty; i++) {
         let numLi = document.createElement("li")
         numLi.innerHTML = i
         numLi.setAttribute("number",i)
